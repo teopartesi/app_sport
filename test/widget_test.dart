d5c6ffc34740ee67a5b7af06cd2e2015e2a3b4eb
teopartesi <teopartesi@gmail.com>
2025-02-04 15:56:21 +0100

    v1.01 = Petite correction de bug sur widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:app_sport/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Test du rendu de l\'application', (WidgetTester tester) async {
+    await tester.pumpWidget(SportApp()); // Assure-toi que c'est bien SportApp() le widget principal
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(BottomNavigationBar), findsOneWidget);
+    expect(find.text("Performances"), findsOneWidget);
+    expect(find.text("Planification"), findsOneWidget);
+    expect(find.text("Profil"), findsOneWidget);
   });
 }
