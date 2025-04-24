b662638653ccba2929289ce21eaa0e6e31e9d236
teopartesi <teopartesi@gmail.com>
2025-04-24 19:24:58 +0200

    v1.4 = ajout d'image pour la page performance et changement de la couleur de base

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,35 @@ class _SportAppState extends State<SportApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(0xFF01BEFD, {
+          50: Color(0xFFE1F7FE),
+          100: Color(0xFFB3ECFD),
+          200: Color(0xFF80E1FC),
+          300: Color(0xFF4DD6FB),
+          400: Color(0xFF26CDFB),
+          500: Color(0xFF00BFFD), // valeur principale
+          600: Color(0xFF01A8E3),
+          700: Color(0xFF018FC7),
+          800: Color(0xFF0176AB),
+          900: Color(0xFF01578A),
+        }),
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.white, // Couleur de fond de l'application
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: Color(0xFF00BFFD), // Couleur des icônes sélectionnées
+          unselectedItemColor: Colors.grey, // Couleur des icônes non sélectionnées
+          backgroundColor: Colors.white, // Couleur de fond du menu
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Color(0xFF00BFFD), // Couleur du texte des boutons interactifs
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Color(0xFF00BFFD), // Couleur du texte des boutons interactifs
+          ),
+        ),
       ),
       home: Scaffold(
         body: _pages[_selectedIndex],
--- lib/pages/performance_page.dart
+++ lib/pages/performance_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import '../models/exercise_structure.dart';
-import 'exercise_list_page.dart'; // Nouvelle page pour afficher les exercices
+import 'exercise_list_page.dart';
 
 class PerformancePage extends StatelessWidget {
+  // Map associant chaque groupe musculaire à son image
+  final Map<String, String> muscleGroupImages = {
+    "Pectoraux": 'assets/icon/chest.png',
+    "Dos": 'assets/icon/back.png',
+    "Épaules": 'assets/icon/shoulders.png',
+    "Biceps": 'assets/icon/biceps.png',
+    "Triceps": 'assets/icon/triceps.png',
+    "Jambes": 'assets/icon/legs.png',
+    "Abdominaux": 'assets/icon/abdos.png',
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +22,21 @@ class PerformancePage extends StatelessWidget {
         itemCount: muscleGroups.length,
         itemBuilder: (context, index) {
           final group = muscleGroups[index];
+          final imagePath = muscleGroupImages[group.name];
 
           return ListTile(
+            leading: imagePath != null
+                ? Image.asset(
+                    imagePath,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.contain,
+                  )
+                : Icon(
+                    Icons.help_outline,
+                    size: 50,
+                    color: const Color.fromARGB(255, 0, 191, 255),
+                  ),
             title: Text(
               group.name,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
